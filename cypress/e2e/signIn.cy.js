0072237a0e63ee0133f7d32d15d001bb339126d1
rvo <rvodoya@gmail.com>
2024-04-22 21:54:58 +0800

    added error test for correct username and wrong password & wrong username and correct password

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -71,6 +71,52 @@ describe('CURA SignIn', () => {
             .wait(500)  
             .should('be.visible')
         })  
+
+        it('User must not be able to log in with wrong username and correct password', () => {
+            
+            cy.url().should('eq', 'https://katalon-demo-cura.herokuapp.com/')
+    
+            cy.get('#menu-toggle > .fa').click()
+    
+            cy.contains('Login').should('be.visible').click()
+    
+            cy.url().should('include','/profile.php#login')
+    
+            uName = "Jose Cruz"
+            pWord = "ThisIsNotAPassword"
+    
+            signIn.enterUserName(uName)
+            signIn.enterPassword(pWord)
+    
+            cy.get('#btn-login').click()
+    
+            cy.xpath("//p[contains(.,'Login failed! Please ensure the username and password are valid.')]")
+            .wait(500)  
+            .should('be.visible')
+        })  
+
+        it('User must not be able to log in with correct username and wrong password', () => {
+            
+            cy.url().should('eq', 'https://katalon-demo-cura.herokuapp.com/')
+    
+            cy.get('#menu-toggle > .fa').click()
+    
+            cy.contains('Login').should('be.visible').click()
+    
+            cy.url().should('include','/profile.php#login')
+    
+            uName = "John Doe"
+            pWord = "This Is A Password"
+    
+            signIn.enterUserName(uName)
+            signIn.enterPassword(pWord)
+    
+            cy.get('#btn-login').click()
+    
+            cy.xpath("//p[contains(.,'Login failed! Please ensure the username and password are valid.')]")
+            .wait(500)  
+            .should('be.visible')
+        })
         
         it('Sign In Correctly', () => {
             uName = "John Doe"
